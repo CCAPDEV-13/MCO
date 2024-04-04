ad14203aad9c4cca76f80eb0110fc24be8328f59
tinySculpture <jmluistro1@gmail.com>
2024-04-04 11:10:41 +0800

    added axios bypass headers

--- src/server/utils/axios.js
+++ src/server/utils/axios.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const http = axios.create({
 	baseURL: process.env.API_URL,
+	headers: {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Headers": "*",
+		"Access-Control-Allow-Methods": "*",
+	}
 })
 
 export default http;
