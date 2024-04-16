89f53be183a531097e69b490c6a4df5736203738
tinySculpture <jmluistro1@gmail.com>
2024-04-16 19:27:11 +0800

    fixed typo

--- src/server/main.js
+++ src/server/main.js
@@ -25,6 +25,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.header("Access-Control-Allow-credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
 })
 app.use(cookieParser());
 
