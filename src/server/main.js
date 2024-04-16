35c34a61b9ada27595e08b6df7bf8c375152eb23
tinySculpture <jmluistro1@gmail.com>
2024-04-16 23:41:57 +0800

    help

--- src/server/main.js
+++ src/server/main.js
@@ -17,9 +17,9 @@ const PORT = 3000;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.url);
-app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
