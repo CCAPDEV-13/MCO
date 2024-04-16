02fc1ce6c863e7520644b4d34a3a491726c2577c
tinySculpture <jmluistro1@gmail.com>
2024-04-16 19:13:55 +0800

    added headers and fixed about page

--- src/client/about.tsx
+++ src/client/about.tsx
@@ -6,6 +6,19 @@ const About = () => {
 			<Navbar />
 			<div className="container" style={{ maxWidth: "85%" }}>
 				<h1>Packages used</h1>
+				<ul>
+					<li>Node</li>
+					<li>React</li>
+					<li>MongoDB</li>
+					<li>Mongoose</li>
+					<li>Axios</li>
+					<li>ReactRouter</li>
+					<li>JSONWebToken</li>
+					<li>Bcrypt</li>
+					<li>React Icons</li>
+					<li>React Markdown</li>
+					<li>Vite</li>
+				</ul>
 			</div>
 		</div>
 	)
