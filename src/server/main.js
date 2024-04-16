13b45b96eafc250b90517a0369c968b4d774beed
tinySculpture <jmluistro1@gmail.com>
2024-04-16 23:39:39 +0800

    made an oopsie

--- src/server/main.js
+++ src/server/main.js
@@ -19,6 +19,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.url);
+app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
