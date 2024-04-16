f765a10044549fe22e588af047daa53b61a8b98b
tinySculpture <jmluistro1@gmail.com>
2024-04-16 23:39:06 +0800

    pls work fr

--- src/server/main.js
+++ src/server/main.js
@@ -18,13 +18,13 @@ const PORT = 3000;
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-  origin: "https://edureview.onrender.com/",
-  Headers: {
-    "Access-Control-Allow-Origin": "*",
-  }
-}));
+app.use(express.url);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 app.use(cookieParser());
 
 mongoose.connect(db_url);
