2eb53110cdd0e670999300e69579e41236e9a5e1
tinySculpture <jmluistro1@gmail.com>
2024-04-16 20:26:10 +0800

    maybe fix

--- src/server/utils/axios.js
+++ src/server/utils/axios.js
@@ -4,6 +4,9 @@ const http = axios.create({
 	baseURL: "http://localhost:3000",
 	headers: {
 		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+		"Access-Control-Allow-credentials": true,
+		"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, UPDATE"
 	}
 })
 
