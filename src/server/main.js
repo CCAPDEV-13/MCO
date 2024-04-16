4e7c08ebe362dde590343b68866053a87d487cb7
tinySculpture <jmluistro1@gmail.com>
2024-04-16 21:33:45 +0800

    changed cors again

--- src/server/main.js
+++ src/server/main.js
@@ -21,16 +21,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: "https://edureview.onrender.com/",
-
+  headers: ["Content-Type"],
+  credentials: true,
 }));
-app.options("*", cors())
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*')
-//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-//   res.header("Access-Control-Allow-credentials", true);
-//   // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, PATCH");
-//   next();
-// })
 app.use(cookieParser());
 
 mongoose.connect(db_url);
