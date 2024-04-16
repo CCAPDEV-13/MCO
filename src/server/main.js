bc7387045427d25b59e3e8307e6a1c83eb39af44
tinySculpture <jmluistro1@gmail.com>
2024-04-16 20:34:35 +0800

    pls fix

--- src/server/main.js
+++ src/server/main.js
@@ -19,14 +19,23 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-credentials", true);
-  // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, PATCH");
-  next();
-})
+app.use(cors({
+  methods: ["GET", "POST", "PUT", "DELETE", "UPDATE", "PATCH"],
+  allowedHeaders: [
+    "Origin",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+    "X-Request-With"
+  ]
+}));
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.header("Access-Control-Allow-credentials", true);
+//   // res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE, PATCH");
+//   next();
+// })
 app.use(cookieParser());
 
 mongoose.connect(db_url);
